c2f8dc5c2b008a89207a2160307e63d21691ac27
Matthew Witte <mcw9612@rit.edu>
2015-08-21 22:44:32 -0400

    Labels and textboxes appear for the number of teams selected

--- WC_Simulator/Form1.cs
+++ WC_Simulator/Form1.cs
@@ -15,6 +15,33 @@ namespace WC_Simulator
         public Form1()
         {
             InitializeComponent();
+            numTeamsDropdown.Items.AddRange(new string[] { "2", "4", "6" });
+        }
+
+        private void continueButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void numTeamsChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                Label label = labelPanel.Controls[i] as Label;
+                TextBox textbox = textboxPanel.Controls[i] as TextBox;
+                if (label != null)
+                    label.Visible = (i < int.Parse(numTeamsDropdown.Text)) ? true : false;
+                if (textbox != null)
+                    textbox.Visible = (i < int.Parse(numTeamsDropdown.Text)) ? true : false;
+            }
+            team1Textbox.Select();
+        }
+
+        private void selectAll(object sender, EventArgs e)
+        {
+            TextBox textbox = sender as TextBox;
+            if (textbox != null)
+                textbox.SelectAll();
         }
     }
 }
